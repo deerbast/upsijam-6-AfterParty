2343778e7c3826d05cc21d075af03666f0545804
Xavier Hueber <xavier.hueber@tutanota.de>
2026-03-21 15:17:09 +0100

    Player speed modifier

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -3,29 +3,44 @@ extends CharacterBody2D
 signal player_moved
 
 var speed = 60
-var TIMER = 0.25
+var NORMAL_SPEED = 60
+var tDASH = 0.25 #Timer
+var tENEMY = 2 #Rateau
+var tDASHTIMEOUT = 5 #Timeout du timer
 
 var grabbed_object: MovableObject = null
 
+@export var dashtimer: Timer
+
 signal dash_signal
+
+func _ready() -> void:
+	dashtimer = $DashTimeout
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
+	
+func speed_modifier() -> float:
+	if grabbed_object != null:
+		return (3 - grabbed_object.masse) * 0.3
+	else:
+		return 1
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = direction * speed
+	velocity = direction * speed * speed_modifier()
 	if move_and_slide():
 		resolve_collisions()
 	emit_signal("player_moved")
 	
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_action_pressed("Dash"):
-		speed = 240
-		dash_signal.emit(TIMER)
+	if event is InputEventKey and event.is_action_pressed("Dash") and $DashTimeout.is_stopped():
+		speed = NORMAL_SPEED * 4
+		$Timer.start(tDASH)
+		$DashTimeout.start(tDASHTIMEOUT)
 
 func _on_timer_timeout() -> void:
-	speed = 60
+	speed = NORMAL_SPEED
 
 #Apply force onto object
 func resolve_collisions() -> void:
@@ -34,3 +49,8 @@ func resolve_collisions() -> void:
 		var body := collision.get_collider() as MovableObject
 		if body:
 			body.apply_impact(velocity)
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if viewport.is_in_group("Enemy"):
+		$Timer.start(tENEMY)
+		speed = NORMAL_SPEED/2
