fdae350120936a81e8205c38d03f59864bba299b
Wisteca <bastian@mecacerf.ch>
2026-03-21 22:43:45 +0100

    fix: player collides with object after throw

--- scenes/objects/object.gd
+++ scenes/objects/object.gd
@@ -25,12 +25,9 @@ func _physics_process(delta: float) -> void:
 	# Set back collisions with player once out of range
 	if _grabbed_by != null and\
 	 _grabbed_by.grabbed_object == null and\
-	!is_overlapping_body(_grabbed_by):
+	!is_overlapping_player(_grabbed_by):
 		await get_tree().physics_frame # Temporize slightly
-		_grabbed_by.remove_collision_exception_with(self)
-		remove_collision_exception_with(_grabbed_by)
-		print("Set back collision between ", self, " and ", _grabbed_by)
-		_grabbed_by = null
+		reset_object_state()
 	
 func apply_impact(impact_velocity: Vector2) -> void:
 	velocity += (impact_velocity - velocity) * IMPACT
@@ -49,6 +46,7 @@ func player_grab_me(player: CharacterBody2D):
 	_grabbed_by = player
 	# Parent and collisions
 	reparent(player)
+	z_index = player.z_index + 1 # Draw above player
 	add_collision_exception_with(player)
 	player.add_collision_exception_with(self)
 	# Move object on top of grabber
@@ -63,6 +61,13 @@ func player_drop_me(player: CharacterBody2D):
 	var mouse_pos = get_global_mouse_position()
 	velocity += global_position.direction_to(mouse_pos) * DROP_FORCE
 	# Collisions are set back once colliders are out 
+	
+func reset_object_state():
+	_grabbed_by.remove_collision_exception_with(self)
+	remove_collision_exception_with(_grabbed_by)
+	z_index = 0
+	print("Set back collision between ", self, " and ", _grabbed_by)
+	_grabbed_by = null
 
 func _input(event: InputEvent) -> void:
 	if !event.is_pressed(): return
@@ -85,20 +90,24 @@ func _input(event: InputEvent) -> void:
 		player_drop_me(player)
 		$Throw.play(0)
 		
-func is_overlapping_body(other: CharacterBody2D) -> bool:
+# This function checks if the object overlaps with the player
+# even when the collision is removed (exception list)
+# It uses the Area2D stored in the player
+func is_overlapping_player(player: CharacterBody2D) -> bool:
 	var space := get_world_2d().direct_space_state
 	assert(collider.shape != null)
+	var overlap = player.get_node("OverlapArea")
+	assert(overlap != null)
 	
 	var query := PhysicsShapeQueryParameters2D.new()
 	query.shape = collider.shape
 	query.transform = collider.global_transform
-	query.collide_with_bodies = true
-	query.collide_with_areas = false
+	query.collide_with_bodies = false
+	query.collide_with_areas = true
 	query.exclude = [self]
 
 	var results := space.intersect_shape(query)
-
 	for result in results:
-		if result.collider == other:
+		if result.collider == overlap:
 			return true
 	return false
