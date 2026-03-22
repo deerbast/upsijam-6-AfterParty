f9964c210fb33f42b9f38975843d79e9b2bbc41e
Wisteca <bastian@mecacerf.ch>
2026-03-22 04:56:39 +0100

    feat: improve fire

--- scenes/firezone/fire_zone.gd
+++ scenes/firezone/fire_zone.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const BURN_S = 0.4
+
 func _ready() -> void:
 	$AnimatedSprite2D.play("default")
 
@@ -7,5 +9,16 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Object"):
 		Global.score += (body.masse + 1) * 3
-		body.queue_free()
-		$Burn.play()
+		body.remove_from_group("Object")
+		burn_object(body)
+		
+func burn_object(body: Node2D):
+	$Burn.play()
+	var tween = create_tween()
+	body.reparent(self)
+	tween.parallel().tween_property(body, "position", Vector2.ZERO, BURN_S)
+	tween.parallel().tween_property(body, "scale", Vector2(0.1, 0.1), BURN_S)
+	tween.parallel().tween_property(body, "modulate:a", 0.0, BURN_S)
+	tween.parallel().tween_property(body, "rotation", randf_range(-1, 1), BURN_S)
+	await tween.finished
+	body.queue_free()
