079ddfaa3f277250522d40cb3aa92f193da9e435
Xavier Hueber <xavier.hueber@tutanota.de>
2026-03-22 09:43:38 +0100

    Add visual timer countdown

--- scenes/upsijam_6.gd
+++ scenes/upsijam_6.gd
@@ -11,15 +11,23 @@ func _ready() -> void:
 	#pass
 	$Player.blocked(5)
 	$Decompte.play("new_animation")
+	$EndOfTime.start(30)
+	$EndOfTime.stop()
 	# Get values for every objects in group for ending screen
 	for obj in get_node("objects").get_children():
 		if obj.is_in_group("Object"):
 			Global.total_possible_score += (obj.masse + 1) * 3 #Modifier 
 
+func _process(_delta: float) -> void:
+	var time = 30
+	if !$EndOfTime.is_stopped():
+		time = int($EndOfTime.time_left)
+	$Player/CanvasLayer/TempsRestant.text = "%ss" % time
+
 func _on_decompte_animation_finished(_anim_name: StringName) -> void:
 	#Game has started, player can move
-	#$EndOfTime.start(30)
-	pass
+	$EndOfTime.start(30)
+	#pass
 
 func _on_end_of_time_timeout() -> void:
 	get_tree().change_scene_to_file("res://scenes/ending/ending.tscn")
