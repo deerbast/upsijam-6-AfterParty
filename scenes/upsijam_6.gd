f8d2a8bf8a123849a9cc9d0bb3926a24c7793a91
Xavier Hueber <xavier.hueber@tutanota.de>
2026-03-22 01:09:38 +0100

    Fix main scene

--- scenes/upsijam_6.gd
+++ scenes/upsijam_6.gd
@@ -3,7 +3,8 @@ extends Node2D
 #To remove for production xD
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_action_pressed("Restart"):
-		get_tree().reload_current_scene()
+		#get_tree().reload_current_scene()
+		get_tree().change_scene_to_file("res://scenes/ending/ending.tscn")
 
 func _ready() -> void:
 	#à activer !!
@@ -13,11 +14,12 @@ func _ready() -> void:
 	# Get values for every objects in group for ending screen
 	for obj in get_node("objects").get_children():
 		if obj.is_in_group("Object"):
-			Global.total_possible_score += obj.masse
+			Global.total_possible_score += (obj.masse + 1) * 3 #Modifier 
 
 func _on_decompte_animation_finished(_anim_name: StringName) -> void:
 	#Game has started, player can move
-	$EndOfTime.start(30)
+	#$EndOfTime.start(30)
+	pass
 
 func _on_end_of_time_timeout() -> void:
 	get_tree().change_scene_to_file("res://scenes/ending/ending.tscn")
