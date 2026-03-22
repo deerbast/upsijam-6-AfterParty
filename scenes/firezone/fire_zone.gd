9406730dbfcb206ebf5b1ab3042c9bae0dd58716
Xavier Hueber <xavier.hueber@tutanota.de>
2026-03-22 01:46:40 +0100

    Fire: add texture and collision

--- scenes/firezone/fire_zone.gd
+++ scenes/firezone/fire_zone.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+func _ready() -> void:
+	$AnimatedSprite2D.play("default")
+
 # Will delete every body on the same layer!
 func _on_body_entered(body: Node2D) -> void:
 	if body.masse != null:
