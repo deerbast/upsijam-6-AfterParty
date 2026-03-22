c0f5a96d2533770857eff5a6a43f34fa97eb77ff
Xavier Hueber <xavier.hueber@tutanota.de>
2026-03-22 10:30:54 +0100

    Progress bar

--- scenes/upsijam_6.gd
+++ scenes/upsijam_6.gd
@@ -12,17 +12,19 @@ func _ready() -> void:
 	$Player.blocked(5)
 	$Decompte.play("new_animation")
 	$EndOfTime.start(30)
-	$EndOfTime.stop()
+	$EndOfTime.stop() 
 	# Get values for every objects in group for ending screen
 	for obj in get_node("objects").get_children():
 		if obj.is_in_group("Object"):
 			Global.total_possible_score += (obj.masse + 1) * 3 #Modifier 
+	$Player/CanvasLayer/ProgressBar.max_value = Global.total_possible_score
 
 func _process(_delta: float) -> void:
 	var time = 30
 	if !$EndOfTime.is_stopped():
 		time = int($EndOfTime.time_left)
 	$Player/CanvasLayer/TempsRestant.text = "%ss" % time
+	$Player/CanvasLayer/ProgressBar.value = Global.score/Global.total_possible_score * 100.0
 
 func _on_decompte_animation_finished(_anim_name: StringName) -> void:
 	#Game has started, player can move
