f1e34fcd608e476f3ee82daf14ffb8e7888d1b50
Xavier Hueber <xavier.hueber@tutanota.de>
2026-03-22 11:27:25 +0100

    Change score to float with 2 decimals

--- scenes/leaderboard/leaderboard.gd
+++ scenes/leaderboard/leaderboard.gd
@@ -5,7 +5,7 @@ func _on_send_to_leaderboard_pressed() -> void:
 		$SendToLeaderboard.disabled = true
 		Global.send = true
 		var player_name = $PlayerName.text
-		var score = int(Global.score/Global.total_possible_score * 100.0)
+		var score = "%.2f" % (Global.score/Global.total_possible_score * 100.0)
 		var sw_result: Dictionary = await SilentWolf.Scores.save_score(player_name, score).sw_save_score_complete
 		print("Score persisted successfully: " + str(sw_result.score_id))
 		update_leaderboard()
@@ -15,9 +15,9 @@ func _ready() -> void:
 	
 func update_leaderboard() -> void:
 	var sw_result: Dictionary = await SilentWolf.Scores.get_scores(0).sw_get_scores_complete
-	var leaderboard = "[table=2][cell expand][center]Username[/center][/cell][cell][center]Score[/center][/cell]"
+	var leaderboard = "[table=2][cell expand][center]  Username  [/center][/cell][cell][center] Score [/center][/cell]"
 	for score in sw_result.scores:
-		leaderboard += "[cell expand][center]%s[/center][/cell][cell][center]%s[/center][/cell]" % [score.player_name, str(int(score.score))]
+		leaderboard += "[cell expand][center] %s [/center][/cell][cell][center] %s [/center][/cell]" % [score.player_name, score.score]
 	leaderboard += "[/table]"
 	$RichTextLabel.text = leaderboard
 	
