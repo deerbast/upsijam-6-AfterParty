041961426c3b7eadc5ea01f5bcd3c1d902097c9b
Xavier Hueber <xavier.hueber@tutanota.de>
2026-03-22 02:18:06 +0100

    Ending: added credits and replay button

--- scenes/ending/ending.gd
+++ scenes/ending/ending.gd
@@ -5,3 +5,8 @@ func _ready() -> void:
 	var score = int(Global.score/Global.total_possible_score * 100.0)
 	var text = "Tu as détruit {nbr}% des resources possibles."
 	$Score.text = text.format({"nbr" : str(score)})
+	$AnimationPlayer.play("new_animation")
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/upsijam_6.tscn")
