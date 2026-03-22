e59d96f5ad09f98298a4977163dba3dfb4750d78
Xavier Hueber <xavier.hueber@tutanota.de>
2026-03-22 09:03:48 +0100

    Titre et phone scene: new textures !

--- scenes/phone/phone.gd
+++ scenes/phone/phone.gd
@@ -3,8 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Button2.disabled = true
-	$Button2.position = Vector2(720,560)
+	$ContinueButton.disabled = true
+	$ContinueButton.position = Vector2(720,560)
 	$Sonnerie_MP3.play(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,13 +13,13 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	$AnimationPlayer.play("RESET")
-	$Button.disabled = true
+	$AnswerButton.disabled = true
 	$Sonnerie_MP3.stop()
 	$MomAudio.play(0)
+	$ContinueButton.position = Vector2(488,512)
 	
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	$Button2.position = Vector2(520,560)
-	$Button2.disabled = false
+	$ContinueButton.disabled = false
 
 func _on_button_2_pressed() -> void:
 	$MomAudio.stop()
