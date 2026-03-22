2cf61151b52226202809dfc5a59a27212a9777cc
Xavier Hueber <xavier.hueber@tutanota.de>
2026-03-22 01:19:08 +0100

    Fix button not working properly

--- scenes/phone/phone.gd
+++ scenes/phone/phone.gd
@@ -4,6 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Button2.disabled = true
+	$Button2.position = Vector2(720,560)
 	$Sonnerie_MP3.play(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,6 +18,7 @@ func _on_button_pressed() -> void:
 	$MomAudio.play(0)
 	
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	$Button2.position = Vector2(520,560)
 	$Button2.disabled = false
 
 func _on_button_2_pressed() -> void:
